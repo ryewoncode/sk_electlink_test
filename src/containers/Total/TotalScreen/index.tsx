3837485db1536481d6a4e65ab0ecdbe5f3ba2c9c
ryewoncode <mykky0919@gmail.com>
2023-07-11 17:14:25 +0900

    feat: 카테고리 '전체' 화면 생성

--- src/containers/Total/TotalScreen/index.tsx
+++ src/containers/Total/TotalScreen/index.tsx
@@ -1,12 +1,20 @@
+import { CustomHeader } from '@/components/CustomHeader';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const TotalScreen = () => {
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Text>TotalScreen</Text>
+    <View style={styles.container}>
+      <CustomHeader title={'전체'} />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>TotalScreen</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#FFF' },
+});
 
 export default TotalScreen;
