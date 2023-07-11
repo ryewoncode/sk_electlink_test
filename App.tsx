53a1f844fd762c916a1d79494e47802286f2ac01
ryewoncode <mykky0919@gmail.com>
2023-07-11 23:02:53 +0900

    feat: 샘플 Lottie를 사용한 샘플 Splash 적용

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Animated, StatusBar, Text, View, useWindowDimensions } from 'react-native';
 import { Provider } from 'react-redux';
 import FastImage from 'react-native-fast-image';
 import Lottie from 'lottie-react-native';
@@ -7,11 +7,31 @@ import createStore from '@/stores';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import Loading from '@/components/Loading';
 import RootScreen from '@/containers/Root/RootScreen';
+import AnimatedLottieView from 'lottie-react-native';
 
 const { store, persistor } = createStore();
 
 const App = () => {
-  return (
+  const { width } = useWindowDimensions();
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 7000);
+  }, []);
+
+  return isVisible ? (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Lottie
+        source={require('@/assets/lottie/Lottie.json')}
+        autoPlay
+        loop={false}
+        resizeMode={'center'}
+        imageAssetsFolder={'images'}
+      />
+    </View>
+  ) : (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <StatusBar barStyle="dark-content" backgroundColor="#fff" />
