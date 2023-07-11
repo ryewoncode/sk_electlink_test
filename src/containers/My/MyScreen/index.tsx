9d691feb9fa539ecb9fd1e3d594293df7fe5489a
ryewoncode <mykky0919@gmail.com>
2023-07-11 17:13:53 +0900

    feat: 나, 스토어 화면 생성

--- src/containers/My/MyScreen/index.tsx
+++ src/containers/My/MyScreen/index.tsx
@@ -1,12 +1,20 @@
+import { CustomHeader } from '@/components/CustomHeader';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const MyScreen = () => {
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Text>MyScreen</Text>
+    <View style={styles.container}>
+      <CustomHeader title={'나'} />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>MyScreen</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#FFF' },
+});
 
 export default MyScreen;
--- src/containers/Store/StoreScreen/index.tsx
+++ src/containers/Store/StoreScreen/index.tsx
@@ -1,12 +1,20 @@
+import { CustomHeader } from '@/components/CustomHeader';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const StoreScreen = () => {
   return (
-    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-      <Text>StoreScreen</Text>
+    <View style={styles.container}>
+      <CustomHeader title={'스토어'} />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>StoreScreen</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#FFF' },
+});
 
 export default StoreScreen;
